9c38fb40307517f04a4e8a7ce6522b1ae7d710ba
Rodrigo da Rosa <irodrigo.rosa@gmail.com>
2020-07-29 23:51:21 -0300

    passando dados do back pro front

---
+++ SegundaVersao/data.js
@@ -0,0 +1,43 @@
+module.exports = [
+  {
+    id: "TBikbn5XJhg",
+    title: "Site próprio - Rodrigo",
+    duration: "57 min",
+    price: "FREE"
+  },
+
+  {
+    id: "6SrsZVdU740",
+    title: "hsauhduashdudhaud hsuhasuhsuahsu ahsuahaushasuhas",
+    duration: "57 min",
+    price: "FREE"
+  },
+
+  {
+    id: "nECTrge3Bw4",
+    title: "Site próprio - Rodrigo",
+    duration: "57 min",
+    price: "FREE"
+  },
+
+  {
+    id: "QvvAIhprVCU",
+    title: "Site próprio - Rodrigo",
+    duration: "57 min",
+    price: "FREE"
+  },
+
+  {
+    id: "uD4izuDMUQA",
+    title: "Site próprio - Rodrigo",
+    duration: "57 min",
+    price: "FREE"
+  },
+
+  {
+    id: "oWTsgofF2Lo",
+    title: "Site próprio - Rodrigo",
+    duration: "57 min",
+    price: "FREE"
+  }
+]
--- SegundaVersao/server.js
+++ SegundaVersao/server.js
@@ -2,20 +2,32 @@ const express = require("express")
 const nunjucks = require("nunjucks")
 
 const server = express()
+const videos = require("./data")
 
 server.use(express.static("public"))
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-  express: server
+  express: server,
+  autoescape: false
 })
 
 server.get("/", function(req, res){
-  return res.render("about")
+  const about = {
+    avatar_url: "https://avatars0.githubusercontent.com/u/38890065?s=460&u=1c03c2ec60d6b3f50e260177acaa2bf4ef563514&v=4",
+    name: "Rodrigo da Rosa",
+    role: 'Trabalho na <a href="https://secullum.com.br" target="_blank">Secullum Softwares</a>',
+    description: "Tentando aprender algo",
+    links: [
+      {name: "Github", url: "https://github.com/RodrigoRosa7", image: "assets/github.png"}
+    ]
+  }
+
+  return res.render("about", {about})
 })
 
 server.get("/portfolio", function(req, res) {
-  return res.render("portfolio")
+  return res.render("portfolio", {items: videos})
 })
 
 server.get("/conteudos", function(req, res) {
